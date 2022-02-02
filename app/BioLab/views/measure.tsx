57dfb432c3eeb805a18d4daecb75fa05c9a73057
MatthiasVandeCasteele <matthias.van.de.casteele@student.ehb.be>
2022-02-02 16:59:16 +0100

    choosing location before choosing camera or incubator and sending data through to camera&devicelist page

--- app/BioLab/views/measure.tsx
+++ app/BioLab/views/measure.tsx
@@ -1,63 +1,177 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Image } from 'react-native';
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  TextInput,
+} from 'react-native';
+import RNFetchBlob from 'rn-fetch-blob';
 
-import { mainStyle, measureStyle, settingsStyle } from '../styles/style';
+import {mainStyle, measureStyle, settingsStyle} from '../styles/style';
 
-export default class Measure extends Component<{ navigation: any }> {
+const userName = 'Matthias';
+
+export default class Measure extends Component<
+  {navigation: any},
+  {
+    locationArray: any[];
+    locationChosen: Boolean;
+    chosenLocation: any[];
+    setLocationName: String;
+    pinName: String;
+    sendingLocationData: any[];
+  }
+> {
   constructor(props: any) {
     super(props);
+    this.displayLocationPins();
+  }
+
+  state = {
+    locationArray: [] as any,
+    locationChosen: false,
+    chosenLocation: [],
+    setLocationName: '',
+    pinName: '',
+    sendingLocationData: [],
+  };
+
+  displayLocationPins() {
+    RNFetchBlob.fetch('GET', `http://10.3.208.84:8100/location/${userName}`, {
+      'Content-Type': 'application/json',
+    }).then(res => {
+      this.setState({locationArray: []});
+      JSON.parse(res.data).forEach((el: any) => {
+        console.log(el);
+        this.state.locationArray.push(
+          // {
+          //   id: el.id,
+          //   locationName: el.locationName
+          // }
+          <View key={el.id}>
+            <TouchableOpacity
+              onPress={() => {
+                this.chooseLocationPin();
+                this.setState({setLocationName: el.locationName});
+              }}>
+              <Text>{el.locationName}</Text>
+            </TouchableOpacity>
+          </View>,
+        );
+      });
+      console.log(this.state.locationArray);
+      this.setState({});
+    });
+  }
+
+  chooseLocationPin() {
+    this.setState({locationChosen: true});
   }
 
   render() {
     return (
       <View style={measureStyle.menuContainer}>
-        {/* Hier komt de body van de pagina */}
-        <Image
-          style={measureStyle.backgroundLogo}
-          source={require('../assets/Analyze_backgroundLogo.png')}
-        />
-        <View style={measureStyle.contentContainer}>
-          <View style={measureStyle.measureRow}>
-            <Text style={measureStyle.subTitle}>Measure with:</Text>
-          </View>
+        {!this.state.locationChosen ? (
+          <>
+            <View>
+              <Text>
+                Choose your sample location or type in a custom location:
+              </Text>
+            </View>
+            <View>{this.state.locationArray}</View>
+            <View>
+              <Text>Choose your own location:</Text>
+              <TextInput
+                onChangeText={e => {
+                  this.setState({pinName: e});
+                }}
+                value={this.state.pinName}></TextInput>
+              <TouchableOpacity
+                onPress={() => {
+                  this.setState({setLocationName: this.state.pinName});
+                  this.chooseLocationPin();
+                }}>
+                <Text>Submit custom location</Text>
+              </TouchableOpacity>
+            </View>
+          </>
+        ) : (
+          <>
+            <Image
+              style={measureStyle.backgroundLogo}
+              source={require('../assets/Analyze_backgroundLogo.png')}
+            />
+            <View style={measureStyle.contentContainer}>
+              <View style={measureStyle.measureRow}>
+                <Text style={measureStyle.subTitle}>Measure with:</Text>
+              </View>
 
-          <View style={measureStyle.row}>
-            <TouchableHighlight
-              activeOpacity={0.5}
-              onPress={() => this.props.navigation.navigate('Device', {})}
-              underlayColor="rgba(255,255,255,0)">
-              <Image
-                source={require('../assets/incubatorChoice.png')}
-                style={measureStyle.images}
-              />
-            </TouchableHighlight>
-            <TouchableHighlight
-              activeOpacity={0.5}
-              onPress={() => this.props.navigation.navigate('Camera', {})}
-              underlayColor="rgba(255,255,255,0)">
-              <Image
-                source={require('../assets/cameraChoice.png')}
-                style={measureStyle.images}></Image>
-            </TouchableHighlight>
-          </View>
-          <View style={measureStyle.row}>
-            <Text style={measureStyle.choiceText}>External</Text>
-            <Text style={measureStyle.choiceText}>Internal</Text>
-          </View>
+              <View style={measureStyle.row}>
+                <TouchableHighlight
+                  activeOpacity={0.5}
+                  onPress={() =>
+                    this.props.navigation.navigate('Device', {
+                      sendingLocationData: this.state.setLocationName,
+                    })
+                  }
+                  underlayColor="rgba(255,255,255,0)">
+                  <Image
+                    source={require('../assets/incubatorChoice.png')}
+                    style={measureStyle.images}
+                  />
+                </TouchableHighlight>
+                <TouchableHighlight
+                  activeOpacity={0.5}
+                  onPress={() =>
+                    this.props.navigation.navigate('Camera', {
+                      sendingLocationData: this.state.setLocationName,
+                    })
+                  }
+                  underlayColor="rgba(255,255,255,0)">
+                  <Image
+                    source={require('../assets/cameraChoice.png')}
+                    style={measureStyle.images}></Image>
+                </TouchableHighlight>
+              </View>
+              <View style={measureStyle.row}>
+                <Text style={measureStyle.choiceText}>External</Text>
+                <Text style={measureStyle.choiceText}>Internal</Text>
+              </View>
 
-          <View style={settingsStyle.settingsBox}>
-            <Text style={settingsStyle.info}>Change your preferences in:</Text>
-            <TouchableHighlight
-              activeOpacity={0.5}
-              onPress={() => this.props.navigation.navigate('SettingsUser', {})}
-              underlayColor="rgba(255,255,255,0)">
-              <Text style={settingsStyle.info}>Settings: Preferences</Text>
-          </TouchableHighlight>
-        </View>
+              <View>
+                <Text>
+                  Your current location is: {this.state.setLocationName}
+                </Text>
+              </View>
 
+              <View>
+                <TouchableOpacity
+                  onPress={() => this.setState({locationChosen: false})}>
+                  <Text>Change location</Text>
+                </TouchableOpacity>
+              </View>
 
+              <View style={settingsStyle.settingsBox}>
+                <Text style={settingsStyle.info}>
+                  Change your preferences in:
+                </Text>
+                <TouchableHighlight
+                  activeOpacity={0.5}
+                  onPress={() =>
+                    this.props.navigation.navigate('SettingsUser', {})
+                  }
+                  underlayColor="rgba(255,255,255,0)">
+                  <Text style={settingsStyle.info}>Settings: Preferences</Text>
+                </TouchableHighlight>
+              </View>
+            </View>
+          </>
+        )}
       </View>
-      </View >
     );
   }
 }
--- app/BioLab/views/user.tsx
+++ app/BioLab/views/user.tsx
@@ -16,176 +16,176 @@ import {
   ImageBackground,
 } from 'react-native';
 
-
 // dependency
 import RNFS from 'react-native-fs';
-import { MMKV } from 'react-native-mmkv'
+// import { MMKV } from 'react-native-mmkv'
 
-const storage = new MMKV()
-
+// const storage = new MMKV()
 
 // userStyle
-import { homeStyle, mainStyle, userStyle } from '../styles/style';
+import {homeStyle, mainStyle, userStyle} from '../styles/style';
 
+export default class User extends Component<{navigation: any}> {
+  state = {
+    userName: '',
+    password: '',
+    connection: '10.3.208.95',
+  };
 
-export default class User extends Component<{ navigation: any }> {
-    state = {
-        userName: '',
-        password: '',
-        connection: '10.3.208.95'
-    }
+  constructor(props: any) {
+    super(props);
+  }
 
-    constructor(props: any) {
-        super(props);
-    }
+  async duplicateUser() {
+    Alert.alert(
+      'Warning',
+      `Account with username:${this.state.userName} already excists! Use different username.`,
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ],
+    );
+  }
 
-    async duplicateUser() {
-        Alert.alert(
-            "Warning",
-            `Account with username:${this.state.userName} already excists! Use different username.`,
-            [
-                {
-                    text: "Cancel",
-                    onPress: () => console.log("Cancel Pressed"),
-                    style: "cancel"
-                },
-                { text: "OK", onPress: () => console.log("OK Pressed") }
-            ]
-        );
-    }
+  async wrongPassword() {
+    Alert.alert('Warning', `Wrong password, try again!`, [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+  }
 
-    async wrongPassword() {
-        Alert.alert(
-            "Warning",
-            `Wrong password, try again!`,
-            [
-                {
-                    text: "Cancel",
-                    onPress: () => console.log("Cancel Pressed"),
-                    style: "cancel"
-                },
-                { text: "OK", onPress: () => console.log("OK Pressed") }
-            ]
-        );
-    }
+  async welcomeMessage() {
+    Alert.alert(
+      `Welcome to the BioLab app ${this.state.userName}.`,
+      `You have now unlimited acces to the app.`,
+      [{text: 'OK', onPress: () => this.props.navigation.navigate('Home', {})}],
+    );
+  }
 
+  async falseUser() {
+    Alert.alert(
+      `No account found with ${this.state.userName}.`,
+      `Try again with different name or create account.`,
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ],
+    );
+  }
 
-    async welcomeMessage() {
-        Alert.alert(
-            `Welcome to the BioLab app ${this.state.userName}.`,
-            `You have now unlimited acces to the app.`,
-            [
-                { text: "OK", onPress: () => this.props.navigation.navigate('Home', {}) }
-            ]
-        );
-    }
+  //   async saveUser() {
+  //     // storage.set('user.name', `${this.state.userName}`);
+  //     // const username = storage.getString('user.name');
+  //     // console.log('Username:', username);
+  //   }
 
-    async falseUser() {
-        Alert.alert(
-            `No account found with ${this.state.userName}.`,
-            `Try again with different name or create account.`,
-            [
-                {
-                    text: "Cancel",
-                    onPress: () => console.log("Cancel Pressed"),
-                    style: "cancel"
-                },
-                { text: "OK", onPress: () => console.log("OK Pressed") }
-            ]
-        );
-    }
+  async createUser() {
+    RNFetchBlob.fetch(
+      'POST',
+      `http://${this.state.connection}:8100/register`,
+      {'Content-Type': 'application/json'},
+      JSON.stringify({
+        userName: this.state.userName,
+        password: this.state.password,
+      }),
+    ).then(res => {
+      let status = res.info().status;
+      if (status == 201) {
+        console.log('Account created!');
+        this.welcomeMessage();
+      } else if (status == 500) {
+        console.log('Fool, account already excists');
+        this.duplicateUser();
+      } else if (status == 400) {
+        console.log("Fetch didn't work");
+      }
+    });
+  }
 
-    async saveUser() {
-        storage.set("user.name", `${this.state.userName}`)
-        const username = storage.getString('user.name')
-        console.log("Username:", username);
-    }
+  async login() {
+    RNFetchBlob.fetch(
+      'POST',
+      `http://${this.state.connection}:8100/login`,
+      {'Content-Type': 'application/json'},
+      JSON.stringify({
+        userName: this.state.userName,
+        password: this.state.password,
+      }),
+    ).then(res => {
+      let status = res.info().status;
+      if (status == 200) {
+        console.log('You are logged in!');
+        // this.saveUser();
+        this.welcomeMessage();
+      } else if (status == 500) {
+        console.log('Fool, wrong password or username');
+        this.wrongPassword();
+      } else if (status == 501) {
+        console.log("Account doesn't excist");
+        this.falseUser();
+      } else if (status == 400) {
+        console.log("Fetch didn't work");
+      }
+    });
+  }
 
+  render(): React.ReactNode {
+    return (
+      <View style={userStyle.container}>
+        <ImageBackground
+          style={userStyle.backgroundGradient}
+          source={require('../assets/gradient.png')}>
+          <View>
+            <Image
+              style={userStyle.logo}
+              source={require('../assets/Logo_noText.png')}
+            />
+          </View>
 
+          <View style={userStyle.textinput}>
+            <View style={userStyle.txtinput}>
+              <TextInput
+                style={userStyle.placeholder}
+                placeholder="EMAIL"
+                onChangeText={text => this.setState({userName: text})}
+              />
+            </View>
+            <View style={userStyle.txtinput}>
+              <TextInput
+                placeholder="PASSWORD"
+                secureTextEntry={true}
+                onChangeText={text => this.setState({password: text})}
+              />
+            </View>
+          </View>
 
+          <View style={userStyle.submitbuttons}>
+            <TouchableHighlight
+              style={userStyle.loginbutton}
+              onPress={() => this.login()}>
+              <Text style={userStyle.loginbuttontxt}>Login</Text>
+            </TouchableHighlight>
 
-    async createUser() {
-        RNFetchBlob.fetch('POST', `http://${this.state.connection}:8100/register`, { 'Content-Type': 'application/json' },
-            JSON.stringify({
-                userName: this.state.userName,
-                password: this.state.password
-            })
-        ).then((res) => {
-            let status = res.info().status;
-            if (status == 201) {
-                console.log("Account created!")
-                this.welcomeMessage();
-            } else if (status == 500) {
-                console.log("Fool, account already excists");
-                this.duplicateUser();
-            } else if (status == 400) {
-                console.log("Fetch didn't work");
-            }
-        })
-    }
-
-    async login() {
-        RNFetchBlob.fetch('POST', `http://${this.state.connection}:8100/login`, { 'Content-Type': 'application/json' },
-            JSON.stringify({
-                userName: this.state.userName,
-                password: this.state.password
-            })
-        ).then((res) => {
-            let status = res.info().status;
-            if (status == 200) {
-                console.log("You are logged in!")
-                this.saveUser();
-                this.welcomeMessage();
-            } else if (status == 500) {
-                console.log("Fool, wrong password or username");
-                this.wrongPassword();
-            } else if (status == 501) {
-                console.log("Account doesn't excist");
-                this.falseUser();
-            }
-            else if (status == 400) {
-                console.log("Fetch didn't work");
-            }
-        })
-    }
-
-
-    render(): React.ReactNode {
-        return (
-            <View style={userStyle.container}>
-                <ImageBackground style={userStyle.backgroundGradient}
-                    source={require("../assets/gradient.png")}>
-                    <View>
-                        <Image style={userStyle.logo} source={require('../assets/Logo_noText.png')} />
-                    </View>
-
-                    <View style={userStyle.textinput}>
-                        <View style={userStyle.txtinput}>
-                            <TextInput style={userStyle.placeholder}
-                                placeholder="EMAIL"
-                                onChangeText={(text) => this.setState({ userName: text })}
-                            />
-                        </View>
-                        <View style={userStyle.txtinput}>
-                            <TextInput
-                                placeholder="PASSWORD"
-                                secureTextEntry={true}
-                                onChangeText={(text) => this.setState({ password: text })}
-                            />
-                        </View>
-                    </View>
-
-                    <View style={userStyle.submitbuttons}>
-                        <TouchableHighlight style={userStyle.loginbutton} onPress={() => this.login()}>
-                            <Text style={userStyle.loginbuttontxt}>Login</Text>
-                        </TouchableHighlight>
-
-                        <TouchableHighlight style={userStyle.registerbutton} onPress={() => this.createUser()}>
-                            <Text style={userStyle.registerbuttontxt} >Register</Text>
-                        </TouchableHighlight>
-                    </View>
-                </ImageBackground>
-            </View >
-        );
-    }
+            <TouchableHighlight
+              style={userStyle.registerbutton}
+              onPress={() => this.createUser()}>
+              <Text style={userStyle.registerbuttontxt}>Register</Text>
+            </TouchableHighlight>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
 }
