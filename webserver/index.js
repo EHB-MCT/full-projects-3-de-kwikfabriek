8d321502ea38baa99ed4a839073ae97ad427319f
MiguelBilliet1995 <miguel.billiet@student.ehb.be>
2022-02-03 14:31:52 +0100

    changing ports

--- webserver/index.js
+++ webserver/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 8100;
+const port = 80;
 const cors = require('cors');
 
 const {
